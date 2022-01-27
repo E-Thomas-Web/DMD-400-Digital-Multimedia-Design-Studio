66099fa07c0ee41d9c94a33cf3d09302ac589c02
Evan T <eth5147@psu.edu>
2022-01-27 13:21:41 -0600

    Building JS

--- Art.js
+++ Art.js
@@ -1,1 +1,15 @@
 console.log('Testing');
+
+function setup() {
+    initializeFields();
+    createCanvas(200, 200);
+}
+
+function draw() {
+    background(50);
+    circle(50, 50, 50);
+}
+
+function initializeFields() {
+}
+
