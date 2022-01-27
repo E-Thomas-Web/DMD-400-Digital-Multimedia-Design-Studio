7c7d5f69e188b9318d25285b33f352e37ebcb3b4
Evan T <eth5147@psu.edu>
2022-01-27 13:13:42 -0600

    Testing JS

---
+++ Art.js
@@ -0,0 +1,14 @@
+
+function setup() {
+    initializeFields();
+    createCanvas(200, 200);
+}
+
+function draw() {
+    background(50);
+    circle(50, 50, 50);
+}
+
+function initializeFields() {
+}
+
